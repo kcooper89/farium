9fb63d5b59ad0a80a3cdbaaed82184abcafc4987
Kyle Cooper <kcooper89.kc@gmail.com>
2020-08-16 17:05:45 -0400

    push 2

--- googlebooks/client/src/Search.js
+++ googlebooks/client/src/Search.js
@@ -17,6 +17,7 @@ class Search extends Component {
     }
 
     handleSave = (book) => {
+        alert()
         fetch('/api/books', {
             method: 'POST',
             body: JSON.stringify(book),
@@ -24,10 +25,6 @@ class Search extends Component {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
               },
-        })
-        .then(() => console.log('Saved'))
-        .catch(error => {
-            console.log(error)
         })
     }
 
@@ -44,15 +41,13 @@ class Search extends Component {
                         <div>{book.volumeInfo.infoLink}</div>
                         <div>{book.volumeInfo?.imageLinks?.thumbnail}</div>
                         <button
-                            onClick={() => {
-                                this.handleSave({
-                                    title: book.volumeInfo.title,
-                                    authors: book.volumeInfo.authors,
-                                    description: book.volumeInfo.description,
-                                    image: book.volumeInfo?.imageLinks?.thumbnail,
-                                    link: book.volumeInfo.infoLink
-                                })
-                            }}
+                            onClick={() => this.handleSave({
+                                title: book.volumeInfo.title,
+                                authors: book.volumeInfo.authors,
+                                description: book.volumeInfo.description,
+                                image: book.volumeInfo?.imageLinks?.thumbnail,
+                                link: book.volumeInfo.infoLink
+                            })}
                         >
                             Save
                         </button>
