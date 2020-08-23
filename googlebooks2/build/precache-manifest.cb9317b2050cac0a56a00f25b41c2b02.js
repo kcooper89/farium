5658cf8d613c310021aabba6b3891a004ae8c14b
Kyle Cooper <kcooper89.kc@gmail.com>
2020-08-23 18:57:18 -0400

    ughh

---
+++ googlebooks2/build/precache-manifest.cb9317b2050cac0a56a00f25b41c2b02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fbb528c779e5b949fdb3e0bbecb257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49dcdafd59123f70f58e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77a589b703e234914bd9",
+    "url": "/static/js/2.428e0144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.428e0144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49dcdafd59123f70f58e",
+    "url": "/static/js/main.b18d9418.chunk.js"
+  },
+  {
+    "revision": "da055993010156fcebc4",
+    "url": "/static/js/runtime-main.40ff41e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
